5adb7339d3a40eb1416dde2158d03441d0c8096e
catexis <anderson.perm@gmail.com>
2019-03-16 11:24:30 +0500

    Requests fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-    $.get("https://catexis.ddns.info:25852/sr");
-    $.get("https://catexis.ddns.info:25852/sc");
+    $.get("http://catexis.ddns.info:25852/sr");
+    $.get("http://catexis.ddns.info:25852/sc");
 });
